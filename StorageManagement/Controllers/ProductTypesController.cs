26d8052cc7613627e036f09c635fbee42ef3452b
Mustafovvv <101758330+Mustafovvv@users.noreply.github.com>
2022-06-12 10:00:06 +0300

    views and controlers

---
+++ StorageManagement/Controllers/ProductTypesController.cs
@@ -0,0 +1,116 @@
+﻿using DataModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageManagement.Controllers
+{
+    public class ProductTypesController : Controller
+    {
+        private readonly StorageDbContext _context;
+        public ProductTypesController(StorageDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult> Index()
+        {
+            var productTypes = _context.ProductTypes.ToList();
+            return View(productTypes);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind("Name,Id")] ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(productType);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductTypes.FindAsync(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Id")] ProductType productType)
+        {
+            if (id != productType.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(productType);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductTypeExists(productType.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(productType);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = await _context.ProductTypes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var productType = await _context.ProductTypes.FindAsync(id);
+            _context.ProductTypes.Remove(productType);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool ProductTypeExists(int id)
+        {
+            return _context.ProductTypes.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ StorageManagement/Controllers/ProductsController.cs
@@ -0,0 +1,151 @@
+﻿using DataModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StorageManagement.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly StorageDbContext _context;
+        public ProductsController(StorageDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult> IndexAsync()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            foreach (var item in products)
+            {
+                item.Staff = _context.Staffs.FirstOrDefault(t => t.Id == item.StaffId);
+                item.Maker = _context.Makers.FirstOrDefault(t => t.Id == item.MakerId);
+                item.ProductType = _context.ProductTypes.FirstOrDefault(t => t.Id == item.ProductTypeId);
+            }
+            return View(products);
+        }
+
+        public ActionResult Details(int id)
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ViewData["ProductTypeId"] = new SelectList(_context.Staffs, "Id", "Name");
+            ViewData["MakerId"] = new SelectList(_context.Makers, "Id", "Country");
+            ViewData["StaffId"] = new SelectList(_context.Staffs, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind("Name,Quantity,Standart,ExpiryDate,ProductTypeId,MakerId,StaffId,Id")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductTypeId"] = new SelectList(_context.Staffs, "Id", "Name",product.ProductTypeId);
+            ViewData["MakerId"] = new SelectList(_context.Makers, "Id", "Country",product.MakerId);
+            ViewData["StaffId"] = new SelectList(_context.Staffs, "Id", "Name",product.StaffId);
+            return View(product);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["ProductTypeId"] = new SelectList(_context.Staffs, "Id", "Name", product.ProductTypeId);
+            ViewData["MakerId"] = new SelectList(_context.Makers, "Id", "Country", product.MakerId);
+            ViewData["StaffId"] = new SelectList(_context.Staffs, "Id", "Name", product.StaffId);
+            return View(product);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Name,Quantity,Standart,ExpiryDate,ProductTypeId,MakerId,StaffId,Id")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductTypeId"] = new SelectList(_context.Staffs, "Id", "Name", product.ProductTypeId);
+            ViewData["MakerId"] = new SelectList(_context.Makers, "Id", "Country", product.MakerId);
+            ViewData["StaffId"] = new SelectList(_context.Staffs, "Id", "Name", product.StaffId);
+            return View(product);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var products = await _context.Products
+                .Include(u => u.Staff)
+                .Include(u => u.ProductType)
+                .Include(u=>u.Maker)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (products == null)
+            {
+                return NotFound();
+            }
+            return View(products);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
